79db0ee286b4b8e44b7e742f3db2ed435866d069
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-06-27 23:56:27 +0530

    Time: 64 ms (21.48%), Space: 151.1 MB (39.72%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,27 +1,38 @@
 class MinStack {
-     stack<pair<int, int>> st;
-
+private:
+    stack<pair<int,int>> st;
+    int current_min; 
 public:
-    void push(int x) {
-        int min;
-        if (st.empty()) {
-            min = x;
-        }
-        else {
-            min = std::min(st.top().second,x);
-        }
-        st.push({x, min});
+    MinStack() {
+        current_min = INT_MAX;
     }
-
+    
+    void push(int val) {
+        if(!st.empty()) current_min = min(val, st.top().second);
+        else current_min = val;
+        st.push({val, current_min});
+    }
+    
     void pop() {
-        st.pop();
+         st.pop();
+         if(st.empty()) current_min =INT_MAX;
     }
-
+    
     int top() {
         return st.top().first;
+        
     }
-
+    
     int getMin() {
         return st.top().second;
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */

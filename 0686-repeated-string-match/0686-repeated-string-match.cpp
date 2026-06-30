c1d47a5de6e38a8050feb79bd0d8ca43ce3f897b
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-06-30 22:24:33 +0530

    Time: 0 ms (100%), Space: 9.7 MB (66.76%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,11 +1,45 @@
 class Solution {
 public:
-   int repeatedStringMatch(string A, string B) {
-    for (auto i = 0, j = 0; i < A.size(); ++i) {
-        for (j = 0; j < B.size() && A[(i + j) % A.size()] == B[j]; ++j);
-        if (j == B.size()) 
-            return (i + j - 1) / A.size() + 1;
+    vector<int> getLps(string a) {
+        vector<int> lps(a.size(), 0);
+        int len = 0;
+        int i = 1;
+        while (i < a.size()) {
+            if (a[i] == a[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } 
+            else {
+                if (len != 0)  len = lps[len - 1];
+                else i++;
+            }
+        }
+        return lps;
     }
-    return -1;
-}
+    int repeatedStringMatch(string s, string p) {
+        int n = ceil(p.size() / s.size()) + 2;
+        cout << n << endl;
+        bool found = false;
+        vector<int> lps = getLps(p);
+        int i = 0;
+        int m = s.size();
+        int j = 0;
+        while (i < n * s.size()) {
+            if (s[i % m] == p[j]) {
+                i++;
+                j++;
+                if (j == p.size()) {
+                    found = true;
+                    break;
+                }
+            } 
+            else {
+                if (j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+        if (!found) return -1;
+        return ceil((i - 1) / m) + 1;
+    }
 };

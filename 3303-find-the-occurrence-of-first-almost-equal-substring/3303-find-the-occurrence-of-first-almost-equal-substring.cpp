018e5971e08cb65460c8288d6e40b270ee07c5a6
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-01 15:33:38 +0530

    Time: 132 ms (93.75%), Space: 77.2 MB (87.5%) - LeetHub

--- 3303-find-the-occurrence-of-first-almost-equal-substring/3303-find-the-occurrence-of-first-almost-equal-substring.cpp
+++ 3303-find-the-occurrence-of-first-almost-equal-substring/3303-find-the-occurrence-of-first-almost-equal-substring.cpp
@@ -1,48 +1,51 @@
 class Solution {
-    public:    
-   vector<int> zArray(string s) {
-    int n = s.size();
-    vector<int> z(n);
-    int l = 0, r = 0;
-    
-    for (int i = 1; i < n; i++) {
-        if (i < r) {
-            z[i] = min(r - i, z[i - l]);
-        }
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-            z[i]++;
-        }
-        if (i + z[i] > r) {
-            l = i;
-            r = i + z[i];
-        }
-    }
-    
-    return z;
-}
 public:
-    int minStartingIndex(string s, string pattern) {
-        string a = pattern + '#' + s;
-        int n = s.size(), m = pattern.size();
-        string b = s + '#' + pattern;
-        reverse(b.begin(), b.end());
-        
-        vector<int> zforward = zArray(a);
-        vector<int> zback = zArray(b);
-    
-        for (int i = m + 1; i < n + m + 1; i++) {
-            if (i + m > n + m + 1) break;
-            int id = i - m - 1;
-            int x = zforward[i];
-            if (x >= m - 1) return id;
-            x++;
-            int req = m - x;
-            int newid = id + m - 1;
-            newid = n - 1 - newid;
-            newid = m + 1 + newid;
-            if (zback[newid] >= req) return id;
+    vector<int> Zfun(string s){
+        int n = s.size();
+        vector<int> z(n, 0);
+        int l = 0, r = 0;
+        z[0] = 0;
+        for(int k=1;k<n;k++){
+            if(k > r){
+                 l = r = k;
+                while(r < n && s[r] == s[r-l]){
+                    r++;
+                }
+                z[k] = r - l;
+                r--;
+            }
+            else{
+                int k1 = k - l;
+                if(z[k1] < r - k + 1){
+                    z[k] = z[k1];
+                }
+                else{
+                    l = k;
+                    while(r  < n && s[r] == s[r-l]) r++;
+                    z[k] = r - l;
+                    r--;
+                }
+            }
         }
-        return -1;
 
+        return z;
     }
+    int minStartingIndex(string s, string p) {
+            int n = s.size();
+            int m = p.size();
+            vector<int> pref = Zfun(p + "$" + s);
+            reverse(p.begin(), p.end());
+            reverse(s.begin(), s.end());
+            vector<int> suf = Zfun(p + "$" + s);
+
+
+            for (int i = 0; i <=n-m; i++) {
+                int prefix = min(pref[m + 1 + i], m);
+                int rev_i  = n - (i + m);            
+                int suffix = min(suf[m + 1 + rev_i], m);
+                if (prefix + suffix >= m - 1)
+                    return i;
+            }
+            return -1;
+    }   
 };

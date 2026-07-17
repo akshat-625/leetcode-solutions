7739db572e1b73072dadc8284e1e5b6a592ed730
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-18 00:05:45 +0530

    Time: 33 ms (35.45%), Space: 14.1 MB (69.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,36 @@
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // sort(nums.begin() , nums.end());
-        int n=nums.size();
+        // TC = O(N^2)
+        int n = nums.size();
         // unordered map<int,int>ans;
-        for( int i=0; i<n; i++){
-            for( int j=i+1; j<n; j++){
-                if(nums[i] + nums[j] == target) return{i,j};
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target)
+                    return {i, j};
             }
         }
         return {};
+
+
+
+//TC=O(N)
+
+unordered_map<int,int> ans;
+
+for( int i=0; i<n; i++){
+    int need = target - nums[i];
+
+if( ans.count(need)){
+    return {ans[need],i};  // here key = need, value = i // because we need i in the answer; also ans[key] is the pinter 
+}
+
+ans[nums[i]] = i;  //just key pointing to its value because  i is asked in the question
+
+}
+return {};
+
     }
 };
+

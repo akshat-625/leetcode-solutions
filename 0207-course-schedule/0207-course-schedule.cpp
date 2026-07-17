660257997302dadeaabd5cd20d4abf62fc036ab7
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-17 21:43:00 +0530

    Time: 7 ms (45.23%), Space: 19.3 MB (68.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<int> adj[n];
-        vector<int> indegree(n, 0);
-        vector<int> ans;
+    bool dfs(vector<vector<int>>& adj, vector<int>& visit, int i) {
+        if (visit[i] == 2) return 0;
+        if (visit[i] == 1) return 1;
+        visit[i] = 1;
 
-        for(auto x: prerequisites){
-            adj[x[0]].push_back(x[1]);
-            indegree[x[1]]++;
+        for (auto j : adj[i]) {
+            if (dfs(adj, visit, j))
+                return true;
         }
+        visit[i] = 2;
+        return false;
+    }
 
-        queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        int m = pre.size();
+        if (m < 2)
+            return 1;
+        vector<vector<int>> adj(n);
+        for (const auto& i : pre) {
+            adj[i[0]].push_back(i[1]);
         }
-
-        while(!q.empty()){
-            auto t = q.front();
-            ans.push_back(t);
-            q.pop();
-
-            for(auto x: adj[t]){
-                indegree[x]--;
-                if(indegree[x] == 0){
-                    q.push(x);
-                }
-            }
+        bool a = 1;
+        vector<int> visit(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == 0 && dfs(adj, visit, i))  return false;
         }
-        return ans.size() == n;
+        return true;
     }
 };

c6719faa8afbc524d33ca6eabdb5fc5bcebc65a2
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-17 21:47:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
+    bool cycleExists(vector<vector<int>>& graph,vector<bool>& visited,vector<bool>& pathVisited,int node)
+    {
+        visited[node]=true;
+        pathVisited[node]=true;
+        for(int i=0;i<graph[node].size();i++)
+        {
+            if(!visited[graph[node][i]])
+            {
+                if(cycleExists(graph,visited,pathVisited,graph[node][i]))
+                return true;
+            }
+            else if(pathVisited[graph[node][i]])
+            return true;
+        }
+        pathVisited[node]=false;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int N = numCourses;
-        vector<int> adj[N];
-
-        for (auto i : prerequisites) {
-            adj[i[0]].push_back(i[1]);
+        vector<vector<int>> graph(numCourses);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            graph[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        vector<int> in(N, 0);
-        for (int i = 0; i < N; i++) {
-            for (auto j : adj[i]) {
-                in[j]++;
+        vector<bool>visited(numCourses,false);
+        vector<bool>pathVisited(numCourses,false);
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!visited[i])
+            {
+                if(cycleExists(graph,visited,pathVisited,i))
+                return false;
             }
         }
-
-        queue<int> q;
-        for (int i = 0; i < N; i++) {
-            if (in[i] == 0)
-                q.push(i);
-        }
-        int c = 0;
-        while (!q.empty()) {
-            int n = q.front();
-            q.pop();
-            c++;
-            for (auto i : adj[n]) {
-                in[i]--;
-                if (in[i] == 0)
-                    q.push(i);
-            }
-        }
-        if (c == N)
-            return 1;
-        return 0;
+        return true;
     }
 };

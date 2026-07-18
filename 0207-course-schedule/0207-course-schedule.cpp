4a79fba076d481b3bd53234a3766d0e6eace9826
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-18 23:39:34 +0530

    Time: 0 ms (100%), Space: 19.1 MB (81.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    bool cycleExists(vector<vector<int>>& graph,vector<bool>& visited,vector<bool>& pathVisited,int node)
-    {
-        visited[node]=true;
-        pathVisited[node]=true;
-        for(int i=0;i<graph[node].size();i++)
-        {
-            if(!visited[graph[node][i]])
-            {
-                if(cycleExists(graph,visited,pathVisited,graph[node][i]))
-                return true;
-            }
-            else if(pathVisited[graph[node][i]])
-            return true;
-        }
-        pathVisited[node]=false;
-        return false;
-    }
+
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        for(int i=0;i<prerequisites.size();i++)
-        {
-            graph[prerequisites[i][0]].push_back(prerequisites[i][1]);
+
+        vector<int> inDegree(numCourses, 0);  // Stores in-degree of each node
+        queue<int> q;                // Queue for processing nodes
+
+        vector<vector<int>>adj(numCourses);
+        for(auto &vec:prerequisites){
+            adj[vec[0]].push_back(vec[1]);
         }
-        vector<bool>visited(numCourses,false);
-        vector<bool>pathVisited(numCourses,false);
-        for(int i=0;i<numCourses;i++)
-        {
-            if(!visited[i])
-            {
-                if(cycleExists(graph,visited,pathVisited,i))
-                return false;
+
+        // Compute in-degree of each node
+        for(int i=0;i<numCourses;i++){
+            for(auto &nbr:adj[i]){
+                inDegree[nbr]++;
             }
         }
-        return true;
+        // Push all nodes with in-degree 0 into the queue
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
+                q.push(i);
+            }
+        }
+
+    int processedNodes = 0;  // Count nodes processed
+
+    // Process nodes using BFS
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        processedNodes++;
+        for(auto &nbr:adj[node]){
+            inDegree[nbr]--;
+            if(inDegree[nbr]==0){
+                q.push(nbr);
+            }
+        }
+    }
+    cout<<processedNodes<<" "<<numCourses<<"\n";
+    // if eqaul mean no cycle
+    return processedNodes==numCourses;
     }
 };

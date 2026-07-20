ddf324893a65dc5a564c84c1ca1b3c13f5be4f86
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-20 23:13:25 +0530

    Time: 7 ms (87.59%), Space: 23.5 MB (55.2%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -1,52 +1,55 @@
 class Solution {
 public:
-    
-    vector<string> funct(int left, int right, string &s, vector<int> &first, vector<int> &last, vector<int> &freq){
-        vector<string> ans;
-
-        for(int i = 0; i<26; i++){
-            if(freq[i] == 0)
-                continue;
-
-            if(left <= first[i] && last[i] <= right){
-                if(freq[i] == last[i] - first[i] + 1)
-                    ans.push_back(s.substr(first[i], last[i]-first[i]+1));
-            }
-        }
-
-        if(ans.size() == 0){
-            while(left <= right && s[left] == s[right] && freq[s[left]-'a'] == 2){
-                left++;
-                right--;
-            }  
-            return {s.substr(left, right-left+1)};
-        }
-
-        return ans;
-    }
-
     vector<string> maxNumOfSubstrings(string s) {
-        int n = s.length();;
-        vector<int> freq(26, 0), first(26, n), last(26, -1);
-        vector<string> ans;
+        int n = s.size();
 
-        for(int i=0; i<n; i++){
-            freq[s[i]-'a']++;
-            first[s[i]-'a'] = min(first[s[i]-'a'], i);
-            last[s[i]-'a'] = i;
+        vector<int> first(26, n), last(26, -1);
+
+        for (int i = 0; i < n; i++) {
+            first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
+            last[s[i] - 'a'] = i;
         }
 
-        int left=0, right = 0;
+        vector<pair<int,int>> intervals;
 
-        for(int i=0; i<n; i++){
-            right = max(right, last[s[i]-'a']);
+        for (int c = 0; c < 26; c++) {
 
-            if(i == right){
-                auto temp = funct(left, right, s, first, last, freq);
-                ans.insert(ans.end(), temp.begin(), temp.end());
-                left = i+1;
+            if (first[c] == n) continue;
+
+            int l = first[c];
+            int r = last[c];
+
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+
+                int ch = s[i] - 'a';
+
+                if (first[ch] < l) {
+                    valid = false;
+                    break;
+                }
+
+                r = max(r, last[ch]);
+            }
+
+            if (valid)
+                intervals.push_back({r, l});
+        }
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<string> ans;
+        int prevEnd = -1;
+
+        for (auto &[r, l] : intervals) {
+
+            if (l > prevEnd) {
+                ans.push_back(s.substr(l, r - l + 1));
+                prevEnd = r;
             }
         }
+
         return ans;
     }
 };

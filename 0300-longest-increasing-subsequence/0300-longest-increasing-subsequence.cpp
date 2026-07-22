f874fdd59b288d37d3c9a8adf55d57c80f95341a
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-22 23:17:34 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        
-        vector<int> dp(n, 1);
-        int maxLen = 1;
-        
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        vector<int> res;
 
-                if (nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
+        for (int n : nums) {
+            if (res.empty() || res.back() < n) {
+                res.push_back(n);
+            } else {
+                int idx = binarySearch(res, n);
+                res[idx] = n;
             }
-            maxLen = max(maxLen, dp[i]);
         }
-        return maxLen;
+
+        return res.size();        
     }
+
+private:
+    int binarySearch(const vector<int>& arr, int target) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }    
 };

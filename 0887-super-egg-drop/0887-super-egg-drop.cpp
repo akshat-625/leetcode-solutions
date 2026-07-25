4424689c17f0d503203798aa4c965bc80e1b0c21
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-25 22:19:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.55%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-   vector<vector<int>> memo;
+    int superEggDrop(int k, int n) {
+        vector<long long> dp(k + 1, 0);
 
-    int find(int k, int n) {
-        if (n == 0 || n == 1) return n;
-        if (k == 1) return n;
+        int moves = 0;
 
-        if (memo[k][n] != -1) return memo[k][n];
+        while (dp[k] < n) {
+            moves++;
 
-        int ans = 1e6;
-        int low = 1, high = n;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            int break_case = find(k - 1, mid - 1); 
-            int survive_case = find(k, n - mid);   
-
-            int temp = 1 + std::max(break_case, survive_case);
-            ans = std::min(ans, temp);
-
-            if (break_case > survive_case) {
-                high = mid - 1; 
-            } else {
-                low = mid + 1;  
+            for (int e = k; e >= 1; e--) {
+                dp[e] = dp[e] + dp[e - 1] + 1;
             }
         }
 
-        return memo[k][n] = ans; 
-    }
-
-    int superEggDrop(int K, int N) {
-        memo.resize(K + 1, std::vector<int>(N + 1, -1));
-        return find(K, N);
+        return moves;
     }
 };

3eefaf83cdbf54d0b9b3928b82483677db614ad0
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-07-26 23:25:13 +0530

    Time: 23 ms (94.24%), Space: 81.2 MB (83.04%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime,
+                      vector<int>& profit) {
+        vector<tuple<int, int, int>> jobs;
         int n = startTime.size();
-        vector<vector<int>> jobs;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             jobs.push_back({endTime[i], startTime[i], profit[i]});
         }
         sort(jobs.begin(), jobs.end());
-        map<int, int> dp = {{0, 0}};
-        for (auto& job : jobs) {
-            int cur = prev(dp.upper_bound(job[1]))->second + job[2];
-            if (cur > dp.rbegin()->second)
-                dp[job[0]] = cur;
+        vector<int> maxi(n, 0);
+        vector<int> dp(n, 0);
+        auto [x, y, z] = jobs[0];
+        dp[0] = maxi[0] = z;
+        for (int i = 1; i < n; i++) {
+            auto [end, st, pr] = jobs[i];
+            int s = 0, e = i - 1, mid;
+            int ans = -1;
+            while (s <= e) {
+                mid = (s + e) / 2;
+                auto [a, b, c] = jobs[mid];
+                if (a > st)
+                    e = mid - 1;
+                else if (a <= st)
+                    s = mid + 1, ans = mid;
+            }
+            if (ans >= 0)
+                dp[i] = pr + maxi[ans];
+            else
+                dp[i] = pr;
+            maxi[i] = max(maxi[i - 1], dp[i]);
         }
-        return dp.rbegin()->second;
+        return maxi[n - 1];
     }
 };

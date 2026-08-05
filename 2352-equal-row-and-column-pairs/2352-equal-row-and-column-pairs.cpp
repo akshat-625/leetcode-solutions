57614579547b65e6685910c1e2f8240b9313ad2c
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-08-05 23:48:53 +0530

    Time: 5 ms (92.19%), Space: 30.1 MB (81.65%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int equalPairs(vector<vector<int>>& grid) 
-    {
-        // Number to store the count of equal pairs.
+    int equalPairs(vector<vector<int>>& grid) {
         int ans = 0;
-        map<vector<int>, int> mp;
-        // Storing each row int he map
-        for (int i = 0; i < grid.size(); i++)
-            mp[grid[i]]++;
-        
-        for (int i = 0; i < grid[0].size(); i++)
-        {
-            vector<int> v;
-            // extracting column in a vector.
-            for (int j = 0; j < grid.size(); j++)
-                v.push_back(grid[j][i]);
-            // Add the number of times that column appeared as a row.
-            ans += mp[v];
+        map<vector<int>,int> mp;
+        for(auto &row : grid){
+            mp[row]++;
         }
-        // Return the number of count
+
+        vector<int> col ;
+        for(int i = 0 ;i<grid[0].size();i++){
+            col.clear();
+            for(int j= 0 ;j<grid.size();j++){
+                col.push_back(grid[j][i]);
+            };
+            ans += mp[col];
+        }
         return ans;
+        
     }
 };

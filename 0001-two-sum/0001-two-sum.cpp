9052102bf54f4d800b3e84a4df2387f14d6de7fd
Akshat Jhanwar <155728582+akshat-625@users.noreply.github.com>
2026-08-20 21:53:38 +0530

    Time: 35 ms (35.85%), Space: 14.2 MB (63.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,38 +1,16 @@
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-
-        // // TC = O(N^2)
-
-        // // unordered map<int,int>ans;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i + 1; j < n; j++) {
-        //         if (nums[i] + nums[j] == target)
-        //             return {i, j};
-        //     }
-        // }
-        // return {};
-
-
-
-//TC=O(N)
-
-unordered_map<int,int> ans;
-
-for( int i=0; i<n; i++) {
-    int need = target - nums[i];
-
-if( ans.count(need)){
-    return {ans[need],i};  // here key = need, value = i // because we need i in the answer; also ans[key] is the pointer 
-}
-
-ans[nums[i]] = i;  //just key pointing to its value because  i is asked in the question
-
-}
-return {};
-
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j}; 
+                }
+            }
+        }
+        
+        return {}; 
     }
 };
-
